15fb0e8329821de0cd0056ad600beb265fb6ef53
Neubi Xavier <neubijr@gmail.com>
2025-05-09 12:37:39 +0000

    Improving the commit message
    This reverts commit fa48b0d6c0b581d1c3bbde21c531daf65655301f

--- battflow/database.py
+++ battflow/database.py
@@ -31,9 +31,10 @@ def dict_null_check(data, path = ""):
     """
     
     for key, value in data.items():
-        parent_path = f"{path}.{key}" if path !="" else key # Can return parent_path if spitting out proeprty is needed
+        parent_path = f"{path}.{key}" if path !="" else key 
         
         if value is None:
+            print(f"{parent_path} is None")
             return True
             
         elif isinstance(value, dict): 
@@ -42,7 +43,7 @@ def dict_null_check(data, path = ""):
                 
     return False
 
-def scan_properties_collection(collection):
+def scan_properties_collection():
     """
     Scan over the the documents in the collection to find if there are properties
     to be calculated. The function will stop in the first missing property found and
@@ -54,18 +55,45 @@ def scan_properties_collection(collection):
             - ObjectID or None: The _id of the document with missing properties as a MongoDB's ObjectID,
               or None if no missing property is found.
     """
+    db, collection = db_connection()
     
     for doc in collection.find():
         print(f"Checking document {doc['_id']} ...")
         
         #add a flag if found
         if dict_null_check(doc["properties"]):
+            print(f"Found properties to calculate in {doc['_id']}")
             doc_id = doc["_id"]
-            print(f"Found missing property in document {doc_id}")
             return True, doc_id # returning both. Remember to unpack tuple later
             
-    print("There are no properties to calculate at this moment")    
-    return False, None #getting out of the if loop to be reachable
+        else:
+            print("There are no properties to calculate at this moment")
+            return False, None
 
+def smiles_setup():
+    """
+    Retrieves and processes molecular components and their SMILES
+    representations from a MongoDB database. It is based on the document
+    output from scan_properties_collection()
+    
+    Returns:
+        A list of tuples containing (component, SMILES) pairs or None if no 
+        document is found.
+    """
+    flag, doc_id = scan_properties_collection();
 
+    if flag:
+        doc = collection.find_one({"_id": doc_id})
+            
+        #electrolyte composition
+        molecules = doc["components"]["molecules"]
+        anions = doc["components"]["anions"]
+        
+        #smiles
+        smiles_molecules = doc["smiles"]["molecules"]
+        smiles_anions = doc["smiles"]["anions"]
+    
+        mols_smiles = list(zip(molecules, smiles_molecules))
+        anions_smiles = list(zip(anions, smiles_anions))
+    return mols_smiles, anions_smiles
 
