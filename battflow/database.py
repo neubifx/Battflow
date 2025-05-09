8b7c8c3ebb023782a0cb6b17142e780264086e73
Neubi Xavier <neubijr@gmail.com>
2025-05-09 12:41:46 +0000

    Included a main run_workflow.py script and fetching bug fixes
    - Introduces a main run_workflow.py file
    - Fixing a bug in the scan_properties_collection function in which False was never being flagged.
    - Separated the smiles/electrolyte setup into md_setup.py file

--- battflow/database.py
+++ battflow/database.py
@@ -31,10 +31,9 @@ def dict_null_check(data, path = ""):
     """
     
     for key, value in data.items():
-        parent_path = f"{path}.{key}" if path !="" else key 
+        parent_path = f"{path}.{key}" if path !="" else key # Can return parent_path if spitting out proeprty is needed
         
         if value is None:
-            print(f"{parent_path} is None")
             return True
             
         elif isinstance(value, dict): 
@@ -43,7 +42,7 @@ def dict_null_check(data, path = ""):
                 
     return False
 
-def scan_properties_collection():
+def scan_properties_collection(collection):
     """
     Scan over the the documents in the collection to find if there are properties
     to be calculated. The function will stop in the first missing property found and
@@ -55,45 +54,18 @@ def scan_properties_collection():
             - ObjectID or None: The _id of the document with missing properties as a MongoDB's ObjectID,
               or None if no missing property is found.
     """
-    db, collection = db_connection()
     
     for doc in collection.find():
         print(f"Checking document {doc['_id']} ...")
         
         #add a flag if found
         if dict_null_check(doc["properties"]):
-            print(f"Found properties to calculate in {doc['_id']}")
             doc_id = doc["_id"]
+            print(f"Found missing property in document {doc_id}")
             return True, doc_id # returning both. Remember to unpack tuple later
             
-        else:
-            print("There are no properties to calculate at this moment")
-            return False, None
+    print("There are no properties to calculate at this moment")    
+    return False, None #getting out of the if loop to be reachable
 
-def smiles_setup():
-    """
-    Retrieves and processes molecular components and their SMILES
-    representations from a MongoDB database. It is based on the document
-    output from scan_properties_collection()
-    
-    Returns:
-        A list of tuples containing (component, SMILES) pairs or None if no 
-        document is found.
-    """
-    flag, doc_id = scan_properties_collection();
 
-    if flag:
-        doc = collection.find_one({"_id": doc_id})
-            
-        #electrolyte composition
-        molecules = doc["components"]["molecules"]
-        anions = doc["components"]["anions"]
-        
-        #smiles
-        smiles_molecules = doc["smiles"]["molecules"]
-        smiles_anions = doc["smiles"]["anions"]
-    
-        mols_smiles = list(zip(molecules, smiles_molecules))
-        anions_smiles = list(zip(anions, smiles_anions))
-    return mols_smiles, anions_smiles
 
